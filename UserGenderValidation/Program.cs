a25bf18cc64d0bec83ce71e321d44844ac1f1953
Roflonzo <rolf.kairo@tktk.ee>
2023-01-20 03:53:58 +0200

    add week2 code

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//rakendus kysib kasutaja sugu
+//rakendus kysib kasutajalt nime
+//lahtudes valikust rakendus tervitab kasutajat:
+//"Welcome,Mr.[kasutaja perekonnanimi]/"Welcome ms. {kasutaja perekonnanimi]"
+
+
+
+Console.WriteLine("Please selest your gender (m/f):");
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter your last name:");
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome,Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
---
+++ week2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, world!");
+// uued andmetyybid
+// if statements (control flow)
